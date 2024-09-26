8ad11f7c248cb1b87b7f0458c5d0e88f070cad2d
mrinmoygithub <mrinmoy.r@xelpmoc.in>
2024-09-26 12:43:37 +0530

    dsa_series_test

---
+++ test_export_hello_world.js
@@ -0,0 +1,6 @@
+// 01_hello_world.js
+
+export default function test() {
+    let testFunction = "Hello World";
+    return testFunction;
+}
---
+++ test_import_print_result.js
@@ -0,0 +1,10 @@
+// 01_print_result.js
+
+// Import the test function from 01_hello_world.js
+import test from './test_export_hello_world.js';
+
+// Call the function and store the result
+const result = test();
+
+// Print the result
+console.log(`Test print message: ${result}`);
